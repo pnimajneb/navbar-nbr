87900effe34593fe33e02ef8ebd1b2c3def42c0f
Benjamin Peters <bpeters@posteo.de>
2024-09-19 13:50:55 +0200

    suggestion putting everything on Nbr view xml

--- webapp/controller/NBR.controller.js
+++ webapp/controller/NBR.controller.js
@@ -4,7 +4,22 @@ sap.ui.define([
     "use strict";
  
     return Controller.extend("sap.ui.demo.nav.controller.Nbr", {
- 
+      pressTile: function(oEvent) {
+         console.log('tile pressed', oEvent);
+      },
+      onTabSelect: function(oEvent) {
+         var oPanel = sap.ui.getCore().byId('container-nav---nbr--PanelTrasse');
+         console.log('Panel selected', oPanel);
+         //oPanel.$().get(0).scrollIntoView({behavior: 'smooth'});
+         var sSearch = oPanel.getHeaderText();
+         var jqTiles = $('.sapMGT');
+         jqTiles.each(function(i, jqTile) {
+            var oTile = sap.ui.getCore().byId(jqTile.id);
+            if (! oTile.getHeader().includes(sSearch)) {
+               oTile.setVisible(false);
+            }
+         }) ;
+      }
     });
  
  });
